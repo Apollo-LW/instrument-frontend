3e2794b96947504a99f15188d7b09a11b0625d18
MrMoon <abuamaramfreelance@gmail.com>
2024-04-12 17:19:06 +0300

    Initial commit

---
+++ src/routes.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+import { Icon } from "@chakra-ui/react";
+import {
+  MdBarChart,
+  MdPerson,
+  MdHome,
+  MdLock,
+  MdOutlineDragHandle,
+} from "react-icons/md";
+
+// Admin Imports
+import MainDashboard from "views/admin/default";
+import NFTMarketplace from "views/admin/marketplace";
+import Profile from "views/admin/profile";
+import DataTables from "views/admin/dataTables";
+
+// Auth Imports
+import SignInCentered from "views/auth/signIn";
+
+const routes = [
+  {
+    name: "Main Dashboard",
+    layout: "/admin",
+    path: "/default",
+    icon: <Icon as={MdHome} width='20px' height='20px' color='inherit' />,
+    component: MainDashboard,
+  },
+  {
+    name: "Courses",
+    layout: "/admin",
+    path: "/courses",
+    icon: (
+      <Icon
+        as={MdOutlineDragHandle}
+        width='20px'
+        height='20px'
+        color='inherit'
+      />
+    ),
+    component: NFTMarketplace,
+    secondary: true,
+  },
+  {
+    name: "Tracking Tables",
+    layout: "/admin",
+    icon: <Icon as={MdBarChart} width='20px' height='20px' color='inherit' />,
+    path: "/tracking-tables",
+    component: DataTables,
+  },
+  {
+    name: "Profile",
+    layout: "/admin",
+    path: "/profile",
+    icon: <Icon as={MdPerson} width='20px' height='20px' color='inherit' />,
+    component: Profile,
+  },
+  {
+    name: "Sign In",
+    layout: "/auth",
+    path: "/sign-in",
+    icon: <Icon as={MdLock} width='20px' height='20px' color='inherit' />,
+    component: SignInCentered,
+  }
+];
+
+export default routes;
