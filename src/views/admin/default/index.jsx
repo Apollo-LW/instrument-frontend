87c65a0a0fc04d9ca7146094c8fb8f0b914739c9
MrMoon <abuamaramfreelance@gmail.com>
2024-04-13 23:10:05 +0300

    Add course counter fetch

--- src/views/admin/default/index.jsx
+++ src/views/admin/default/index.jsx
@@ -1,31 +1,22 @@
 // Chakra imports
 import {
-  Avatar,
   Box,
   Flex,
-  FormLabel,
   Icon,
-  Select,
   SimpleGrid,
   useColorModeValue,
 } from "@chakra-ui/react";
 // Assets
-import Usa from "assets/img/dashboards/usa.png";
 // Custom components
 import MiniCalendar from "components/calendar/MiniCalendar";
 import MiniStatistics from "components/card/MiniStatistics";
 import IconBox from "components/icons/IconBox";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import {
   MdAddTask,
-  MdAttachMoney,
-  MdBarChart,
   MdFileCopy,
 } from "react-icons/md";
-import CheckTable from "views/admin/default/components/CheckTable";
 import ComplexTable from "views/admin/default/components/ComplexTable";
-import DailyTraffic from "views/admin/default/components/DailyTraffic";
-import PieCard from "views/admin/default/components/PieCard";
 import Tasks from "views/admin/default/components/Tasks";
 import TotalSpent from "views/admin/default/components/TotalSpent";
 import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
@@ -33,13 +24,27 @@ import {
   columnsDataCheck,
   columnsDataComplex,
 } from "views/admin/default/variables/columnsData";
-import tableDataCheck from "views/admin/default/variables/tableDataCheck.json";
 import tableDataComplex from "views/admin/default/variables/tableDataComplex.json";
 
 export default function UserReports() {
   // Chakra Color Mode
   const brandColor = useColorModeValue("brand.500", "white");
   const boxBg = useColorModeValue("secondaryGray.300", "whiteAlpha.100");
+
+  const [numberOfCourses, setNumberOfCourses] = useState(0);
+
+  useEffect(() => {
+    fetchNumberOfCourses();
+  }, []);
+
+  const fetchNumberOfCourses = async () => {
+    const response = await fetch("http://localhost:3000/course/user/userId");
+    const x = await response.json();
+    console.log(x);
+    setNumberOfCourses(x);
+    //setNumberOfCourses(data); //Setting the response into state
+  };
+
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       <SimpleGrid
@@ -67,7 +72,7 @@ export default function UserReports() {
             </Flex>
           }
           name='2024 Courses'
-          value='10'
+          value={numberOfCourses}
         />
         <MiniStatistics
           startContent={
