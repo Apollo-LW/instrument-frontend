49810b2f52c737a8f5f28f39b7f4779a28e62b84
MrMoon <abuamaramfreelance@gmail.com>
2024-04-15 16:24:23 +0300

    Add profile fullname username email

--- src/views/admin/profile/index.jsx
+++ src/views/admin/profile/index.jsx
@@ -1,25 +1,3 @@
-/*!
-  _   _  ___  ____  ___ ________  _   _   _   _ ___   
- | | | |/ _ \|  _ \|_ _|__  / _ \| \ | | | | | |_ _| 
- | |_| | | | | |_) || |  / / | | |  \| | | | | || | 
- |  _  | |_| |  _ < | | / /| |_| | |\  | | |_| || |
- |_| |_|\___/|_| \_\___/____\___/|_| \_|  \___/|___|
-                                                                                                                                                                                                                                                                                                                                       
-=========================================================
-* Horizon UI - v1.1.0
-=========================================================
-
-* Product Page: https://www.horizon-ui.com/
-* Copyright 2023 Horizon UI (https://www.horizon-ui.com/)
-
-* Designed and Coded by Simmmple
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
-
 // Chakra imports
 import { Box, Grid } from "@chakra-ui/react";
 
@@ -34,9 +12,35 @@ import Upload from "views/admin/profile/components/Upload";
 // Assets
 import banner from "assets/img/auth/banner.png";
 import avatar from "assets/img/avatars/avatar4.png";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function Overview() {
+  const [username, setUsername] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const fetchFullName = async () => {
+    const response = await axios.get("http://localhost:3000/user/username/MrMoon", {
+      headers: {
+        "Authorization" : `Bearer ${localStorage.getItem("token")}`,
+      }
+    });
+
+    const x = response.data;
+    if (x) {
+      setFullName(`${x.firstName} ${x.lastName}`);
+      setUsername(x.username);
+      setEmail(x.email);
+      console.log(x.email);
+      console.log(x.username);
+    }
+  };
+
+  useEffect(() => {
+    fetchFullName();
+  }, []);
+
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
       {/* Main Fields */}
@@ -54,8 +58,8 @@ export default function Overview() {
           gridArea='1 / 1 / 2 / 2'
           banner={banner}
           avatar={avatar}
-          name='Adela Parkson'
-          job='Product Designer'
+          name={fullName}
+          user={username}
           posts='17'
           followers='9.7k'
           following='274'
