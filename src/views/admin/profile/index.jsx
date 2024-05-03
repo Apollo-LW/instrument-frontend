716e4ec9303c499c482a94fd4dd0b41567e55ad8
MrMoon <abuamaramfreelance@gmail.com>
2024-05-03 18:26:05 +0300

    Add delete user button

--- src/views/admin/profile/index.jsx
+++ src/views/admin/profile/index.jsx
@@ -1,5 +1,5 @@
 // Chakra imports
-import { Box, Grid } from "@chakra-ui/react";
+import { AlertDialog, AlertDialogBody, AlertDialogContent, AlertDialogFooter, AlertDialogHeader, AlertDialogOverlay, Box, Button, Grid, useColorModeValue, useDisclosure } from "@chakra-ui/react";
 
 // Custom components
 import Banner from "views/admin/profile/components/Banner";
@@ -14,9 +14,13 @@ import banner from "assets/img/auth/banner.png";
 import avatar from "assets/img/avatars/avatar4.png";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 export default function Overview() {
   const [username, setUsername] = useState("");
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = React.useRef()
+  const history = useHistory();
 
   const fetchFullName = async () => {
     const response = await axios.get(`http://localhost:3000/user/${localStorage.getItem("userId")}`, {
@@ -30,6 +34,22 @@ export default function Overview() {
       setUsername(x.username);
     }
   };
+
+  const deleteAccount = async () => {
+    const response = await axios.delete(`http://localhost:3000/user/${localStorage.getItem("userId")}`, {
+      headers: {
+        "Authorization" : `Bearer ${localStorage.getItem("token")}`,
+      }
+    });
+
+    const x = response.data;
+    if (x) {
+      console.log(x);
+      localStorage.removeItem("token");
+      localStorage.removeItem("userId");
+      history.push('/auth');
+    }
+  }
 
   useEffect(() => {
     fetchFullName();
@@ -97,6 +117,44 @@ export default function Overview() {
           minH='365px'
           pe='20px'
         />
+        <Button
+              me='100%'
+              mb='50px'
+              w='340px'
+              minW='340px'
+              mt={{ base: "20px", "2xl": "auto" }}
+              variant='brand'
+              onClick={onOpen}
+              background="red"
+              fontWeight='500'>
+              Delete Account
+          </Button>
+          <AlertDialog
+            isOpen={isOpen}
+            leastDestructiveRef={cancelRef}
+            onClose={onClose}
+          >
+            <AlertDialogOverlay>
+              <AlertDialogContent>
+                <AlertDialogHeader fontSize='lg' fontWeight='bold'>
+                  Delete Customer
+                </AlertDialogHeader>
+
+                <AlertDialogBody>
+                  Are you sure? You can't undo this action afterwards.
+                </AlertDialogBody>
+
+                <AlertDialogFooter>
+                  <Button ref={cancelRef} onClick={onClose}>
+                    Cancel
+                  </Button>
+                  <Button colorScheme='red' onClick={deleteAccount} ml={3}>
+                    Delete
+                  </Button>
+                </AlertDialogFooter>
+              </AlertDialogContent>
+            </AlertDialogOverlay>
+          </AlertDialog>
       </Grid>
     </Box>
   );
